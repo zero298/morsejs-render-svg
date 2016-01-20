645e60ee06dda3b814d6c2b131012fdcd0d36c6a
Stephen K <zero298@gmail.com>
2016-01-19 22:35:03 -0600

    Fixed coverage metric and added test SVG clear
    + Added istanbul directive to fix coverage metric
    + Updated which morsejs is needed
    + Added a test to ensure that clearing the SVG element works

--- test/index.js
+++ test/index.js
@@ -23,4 +23,49 @@ describe("Test SVG rendering", function () {
         expect(circleEls.length).toEqual(6);
         expect(lineEls.length).toEqual(3);
     });
+
+    it("Makes sure that the SVG element is cleared before we render the message", function () {
+        var svgEl = document.createElement("svg"),
+            message1 = morsejs.translate("sos"),
+            message2 = morsejs.translate("test"),
+            circleEls,
+            lineEls;
+
+        // Append our test SVG element
+        document.body.appendChild(svgEl);
+        
+        /*
+         * The first message should insert whatever
+         * dots and lines are necessary to make the message
+         * above
+         */
+        
+        // Render our message the first time to make sure that there are elements there
+        morsejsRenderSVG.graphMorse(svgEl, message1);
+
+        // Get our circles and lines
+        circleEls = svgEl.querySelectorAll("circle");
+        lineEls = svgEl.querySelectorAll("line");
+
+        // Make sure we have the right ammount
+        expect(circleEls.length).toEqual(6);
+        expect(lineEls.length).toEqual(3);
+        
+        /*
+         * The second message should first CLEAR OUT the SVG
+         * and THEN insert dots and lines are necessary to 
+         * make the second message above
+         */
+        
+        // Render our message the first time to make sure that there are elements there
+        morsejsRenderSVG.graphMorse(svgEl, message2);
+
+        // Get our circles and lines
+        circleEls = svgEl.querySelectorAll("circle");
+        lineEls = svgEl.querySelectorAll("line");
+
+        // Make sure we have the right ammount
+        expect(circleEls.length).toEqual(4);
+        expect(lineEls.length).toEqual(2);
+    });
 });
